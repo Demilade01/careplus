b385d4b87dd61fda123c408b9394f120f4dd6d10
Demilade01 <demiladeakinyemi2004@gmail.com>
2025-01-19 13:37:17 -0800

    update ui for auth form

--- components/form/PatientForm.tsx
+++ components/form/PatientForm.tsx
@@ -6,6 +6,8 @@ import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -22,6 +24,8 @@ const formSchema = z.object({
 })
 
 const PatientForm = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -65,11 +69,13 @@ const PatientForm = () => {
           control={form.control}
           name="PHONE"
           label="Phone Number"
-          placeholder="(555) 103 4471"
+          placeholder="(555) 103 4471 "
           iconSrc="/assets/icons/email.svg"
           iconAlt="email"
         />
-        <Button type="submit" className="text-white">Submit</Button>
+        <SubmitButton isLoading={isLoading}>
+          Get Started
+        </SubmitButton>
       </form>
     </Form>
   )
