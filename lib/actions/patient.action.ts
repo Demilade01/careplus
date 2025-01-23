ce37ac843bab27125d1b1305ad29f9c5e1ea028a
Demilade01 <demiladeakinyemi2004@gmail.com>
2025-01-23 13:21:22 -0800

    implement user registration form

--- lib/actions/patient.action.ts
+++ lib/actions/patient.action.ts
@@ -1,6 +1,9 @@
+"use server"
+
 import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+import { BUCKET_ID, databases, PROJECT_ID, storage, users } from "../appwrite.config"
 import { parseStringify } from "../utils"
+import { InputFile } from "node-appwrite/file"
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -32,5 +35,38 @@ export const getUser = async (userId: string) => {
     return parseStringify(user);
   } catch (error) {
     console.log(error);
+  }
+}
+
+export const registerPatient = async ({ identificationDocument, ...patient}: RegisterUserParams) => {
+  try {
+    let file;
+
+    if(identificationDocument) {
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get('blobFile') as Blob,
+        identificationDocument?.get('fileName') as string,
+      )
+
+      file = await storage.createFile("678d7a8c0006f9a5628e",
+        ID.unique(),
+        inputFile
+      )
+    }
+
+    const newPatient = await databases.createDocument(
+      process.env.DATABASE_ID || "678d76bb001a4af7d985",
+      process.env.PATIENT_COLLECTION_ID || "678d79c800227732cdd4",
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id || null,
+        identificationDocumentUrl: `https://cloud.appwrite.io/v1/storage/buckets/${BUCKET_ID! || "678d7a8c0006f9a5628e"}//${file?.$id}/view?project=${PROJECT_ID || "678d75c10039401e32f0"}`,
+        ...patient
+      }
+    )
+
+    return parseStringify(newPatient)
+  } catch (error) {
+    console.log(error)
   }
 }
